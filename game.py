5d31a081fe4902e542ac7b1a395798e37090663d
Renáta <131404764+Ren6436@users.noreply.github.com>
2025-06-15 10:53:14 +0200

    Spawn (#11)

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 """Game module"""
 
+
 import pygame
+import random
+
 
 from bullet import Bullet
 from enums import KeyType
@@ -8,14 +11,19 @@ from inputs import InputManager, PLAYER_KEYMAPS
 from player import Player
 from npc import NPC
 
+
 from config import GAME_FPS
+
+
 
 
 class AbstractGame:
     """Abstract game ancestor"""
 
+
     def __init__(self, **kwargs):
         self.input_manager = InputManager()
+
 
         self.screen = kwargs["screen"]
         self.clock = pygame.time.Clock()
@@ -24,12 +32,37 @@ class AbstractGame:
         self.players = {}
         self.npcs = []                    # todo DICT by id????
 
+
         self.bullets = []
         self.player_bullets = []
         self.npc_bullets = []
 
+
         self.npc_last_shot_times = {}
         self.npc_shoot_cooldown = 500
+        self.score = 0
+        self.last_spawn_time = pygame.time.get_ticks()
+        self.spawn_interval_range = (3000, 5000)
+
+
+    def spawn_random_npc(self):
+        npc_type = random.choices(
+            ["easy", "medium", "hard"],
+            weights=[70, 25, 5],
+            k=1
+        )[0]
+        x = random.randint(50, self.screen.get_width() - 50)
+        y = random.randint(50, self.screen.get_height() - 50)
+        self.npcs.append(NPC(x, y, npc_type=npc_type))
+
+
+    def try_spawn_npc(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_spawn_time > self.next_spawn_interval:
+            self.spawn_random_npc()
+            self.last_spawn_time = now
+            # Nastav nový random interval pro další spawn
+            self.next_spawn_interval = random.randint(*self.spawn_interval_range)
 
 
     def try_npc_shoot(self, npc):
@@ -39,11 +72,39 @@ class AbstractGame:
         if now - last_shot >= self.npc_shoot_cooldown:
             target = npc.get_shot_target(self.players.values())
             if target:
-                bullet = Bullet(npc.rect.centerx, npc.rect.centery,
-                                target.rect.centerx, target.rect.centery,
-                                color=(255, 0, 0))
+                bullet = Bullet(
+                    npc.rect.centerx, npc.rect.centery,
+                    target.rect.centerx, target.rect.centery,
+                    color=(255, 0, 0),
+                    shooter=npc  # tady přidáš referenci na střílejícího NPC
+                )
                 self.npc_bullets.append(bullet)
                 self.npc_last_shot_times[npc_id] = now
+
+
+    def check_bullet_collisions(self):
+        # NPC střely na hráče
+        for bullet in self.npc_bullets[:]:
+            for npc in self.npcs:
+                if bullet.shooter == npc:
+                    for player in self.players.values():
+                        if bullet.rect.colliderect(player.rect):
+                            player.health -= npc.damage
+                            self.npc_bullets.remove(bullet)
+                            break
+                    break
+
+
+        for bullet in self.player_bullets[:]:
+            for npc in self.npcs[:]:
+                if bullet.rect.colliderect(npc.rect):
+                    npc.health -= 10
+                    if npc.health <= 0:
+                        self.npcs.remove(npc)
+                        self.score += npc.score
+                    self.player_bullets.remove(bullet)
+                    break
+
 
     def handle_events(self):
         """Handles events and interruptions"""
@@ -52,51 +113,74 @@ class AbstractGame:
                 print("Pressed quitting!")
                 self.running = False
 
+
     def handle_key_events(self):
         """Handles key presses"""
         raise NotImplementedError
 
 
+
+
     def update_players(self,  **kwargs):
         """Handles players updates"""
 
+
         for player in self.players.values():
+
 
             inputs = self.input_manager.get_inputs(player.uid)
 
+
             player.update(inputs = inputs, **kwargs)
+
 
             if KeyType.SHOOT.name in inputs:
                 bullet = player.shoot(self.npcs)
                 if bullet is not None:
                     self.player_bullets.append(bullet)
 
+
             self.input_manager.clear_inputs(player.uid)
+
+
 
 
     def update_npcs(self,  **kwargs):
         """Handles npcs updates"""
+
 
         for npc in self.npcs:
             npc.update(players = self.players, **kwargs)
             self.try_npc_shoot(npc)
 
 
+
+
     def render_all(self):
         """Draws itself"""
         self.screen.fill((0, 0, 0))
 
+
         for player in self.players.values():
             player.draw(self.screen)
+
 
         for npc in self.npcs:
             npc.draw(self.screen)
 
+
         for bullet in self.player_bullets:
             bullet.draw(self.screen)
 
+
         for bullet in self.npc_bullets:
             bullet.draw(self.screen)
+
+
+        font = pygame.font.SysFont(None, 30)
+        score_surface = font.render(f"Score: {self.score}", True, (255, 255, 255))
+        self.screen.blit(score_surface, (10, 10))
+
 
     def update_bullets(self):
         """Update bullets position"""
@@ -106,84 +190,136 @@ class AbstractGame:
                 if bullet.is_off_screen():
                     bullet_list.remove(bullet)
 
+    def check_game_end(self):
+        alive_players = [p for p in self.players.values() if p.health > 0]
+        if not alive_players:
+            self.running = False
+            if self.score >= 200:
+                self.display_end_message("You win!")
+            else:
+                self.display_end_message("You lost.")
+
+    def display_end_message(self, message):
+        font = pygame.font.SysFont(None, 60)
+        text = font.render(message, True, (255, 255, 255))
+        rect = text.get_rect(center=(self.screen.get_width() // 2, self.screen.get_height() // 2))
+        self.screen.fill((0, 0, 0))
+        self.screen.blit(text, rect)
+        pygame.display.flip()
+        pygame.time.wait(3000)
+
 
     def run(self):
         """Running loop"""
+        self.next_spawn_interval = random.randint(*self.spawn_interval_range)
         while self.running:
             self.handle_events()
             self.handle_key_events()
+
+
+            self.try_spawn_npc()
+
 
             self.update_players()
             self.update_npcs()
             self.update_bullets()
 
+
             self.render_all()
             pygame.display.flip()
             self.clock.tick(self.tick)
+            self.check_bullet_collisions()
+            self.check_game_end()
+
 
         print("Closing game ....")
+
+
 
 
 class SingleGame(AbstractGame):
     """Single player game"""
 
+
     PLAYER1 = "Player1"
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
 
         pl1 = Player(self.PLAYER1)       # todo some better init
         pl1.set_coords(20,20)
         self.players[self.PLAYER1] = pl1
 
+
         self.input_manager.add_keymap(self.PLAYER1, PLAYER_KEYMAPS["wasd"])
 
-        self.npcs.append(NPC(400, 400))
+
+        self.spawn_random_npc()
+
 
     def handle_key_events(self):
         """Handles key presses"""
 
+
         keys = pygame.key.get_pressed()
+
 
         for key in PLAYER_KEYMAPS["wasd"].keys():
             if keys[key]:
                 self.input_manager.add_input(self.PLAYER1, key)
 
 
+
+
 class CoopGame(AbstractGame):
     """Single player game"""
+
 
     PLAYER1 = "Player1"
     PLAYER2 = "Player2"
 
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
 
         pl1 = Player(self.PLAYER1)
         pl1.set_coords(20,20)
         self.players[self.PLAYER1] = pl1
 
+
         self.input_manager.add_keymap(self.PLAYER1, PLAYER_KEYMAPS["wasd"])
+
 
         pl2 = Player(self.PLAYER2)  # experimental player 2
         pl2.set_coords(100, 20)
         pl2.color = (0,0,255)
         self.players[self.PLAYER2] = pl2
 
+
         self.input_manager.add_keymap(self.PLAYER2, PLAYER_KEYMAPS["arrows"])
 
+
         self.npcs.append(NPC(400, 400))
+
+
 
 
     def handle_key_events(self):
         """Handles key presses"""
 
+
         keys = pygame.key.get_pressed()
+
 
         for key in PLAYER_KEYMAPS["wasd"].keys():
             if keys[key]:
                 self.input_manager.add_input(self.PLAYER1, key)
 
+
         for key in PLAYER_KEYMAPS["arrows"].keys():
             if keys[key]:
                 self.input_manager.add_input(self.PLAYER2, key)
+
--- player.py
+++ player.py
@@ -2,15 +2,19 @@
 import math
 import pygame
 
+
 from config import PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT
 from enums import KeyType, Facing
 from bullet import Bullet
 
+
 movement_keys = {KeyType.LEFT.name, KeyType.RIGHT.name, KeyType.UP.name, KeyType.DOWN.name}
+
 
 class Player:
     """Player class init"""
     def __init__(self, uid):
+
 
         self.uid = uid
         self.rect = pygame.Rect(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT)
@@ -19,26 +23,37 @@ class Player:
         self.speed = PLAYER_SPEED
         self.is_moving = False
         self.facing = Facing.RIGHT
+        self.health = 100
+        self.max_health = 100
+
+
 
 
         self.shoot_cooldown = 250
         self.last_shot_time = pygame.time.get_ticks()
 
 
+
+
     def set_coords(self, x, y):
         """Coords setter"""
         self.rect = pygame.Rect(x, y, PLAYER_WIDTH, PLAYER_HEIGHT)
+
 
     def set_name(self, name):
         """Name setter"""
         self.name = name
 
+
     def update(self, **kwargs):
         """Update self position from move intention"""
 
+
         inp = kwargs["inputs"]
 
+
         self.is_moving = any(key in inp for key in movement_keys)
+
 
         if KeyType.UP.name in inp:
             self.rect.y -= self.speed
@@ -47,6 +62,7 @@ class Player:
             self.rect.y += self.speed
             self.facing = Facing.DOWN
 
+
         if KeyType.LEFT.name in inp:
             self.rect.x -= self.speed
             self.facing = Facing.LEFT
@@ -54,19 +70,23 @@ class Player:
             self.rect.x += self.speed
             self.facing = Facing.RIGHT
 
+
         # overflow corrections - todo možná udělat přes kolize s okrajem!
         self.rect.x = max(self.rect.x, 0)
         self.rect.y = max(self.rect.y, 0)
         self.rect.x = min(self.rect.x, SCREEN_WIDTH - PLAYER_WIDTH)
         self.rect.y = min(self.rect.y, SCREEN_HEIGHT - PLAYER_HEIGHT)
 
+
     def distance(self, rect1, rect2):
         """Helper function for distance computing"""
         return math.hypot(rect1.x - rect2.x, rect1.y - rect2.y)
 
+
     def find_closest_npc(self, npcs):
         closest = None
         min_dist = float('inf')
+
 
         for npc in npcs:
             dist = self.distance(self.rect, npc.rect)
@@ -74,11 +94,15 @@ class Player:
                 min_dist = dist
                 closest = npc
 
+
         return closest
+
+
 
 
     def shoot(self, npcs):
         """Try to shoot, does not fire in cooldown"""
+
 
         now = pygame.time.get_ticks()
         if now - self.last_shot_time >= self.shoot_cooldown:
@@ -86,12 +110,25 @@ class Player:
             if target is None:
                 return None
 
+
             target_pos = (target.rect.centerx, target.rect.centery)
             bullet = Bullet(self.rect.centerx, self.rect.centery, *target_pos, color=self.color)
             self.last_shot_time = now
             return bullet
 
 
+    def draw_lifebar(self, screen):
+        bar_width = self.rect.width
+        bar_heigth = 5
+        fill = (self.health / self.max_health) * bar_width
+
+
+        pygame.draw.rect(screen, (255, 0, 0), (self.rect.x, self.rect.y - 10, bar_width, bar_heigth))
+        pygame.draw.rect(screen, (0, 255, 0), (self.rect.x, self.rect.y - 10, fill, bar_heigth))
+
+
     def draw(self, screen):
         """Draws itself"""
         pygame.draw.rect(screen, self.color, self.rect)
+        self.draw_lifebar(screen)
+
